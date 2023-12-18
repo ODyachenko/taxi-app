efa74a721831d807b527e10bad8b05893130f5e5
ODyachenko <oleg1696@gmail.com>
2023-12-18 13:39:57 +0200

    Redux Toolkit init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import ReduxProvider from '@/redux/Provider';
 import './globals.scss';
 
 const inter = Inter({
@@ -20,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
     </html>
   );
 }
