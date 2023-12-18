57998674ab0defe57009cb4f74f607662b2241fb
ODyachenko <oleg1696@gmail.com>
2023-12-18 13:47:19 +0200

    Supabase init

---
+++ supabase/settings.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_supabaseUrl,
+  process.env.NEXT_PUBLIC_supabaseKey
+);
