13639843c0c5c7a248a22534e821d7c878199cdb
ODyachenko <oleg1696@gmail.com>
2023-12-18 12:07:07 +0200

    Splash screen markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Btn from '@/UI/Btn/Btn';
+import Logo from '@/UI/Btn/Logo';
 import Image from 'next/image';
 
 export default function Home() {
---
+++ app/splash/page.tsx
@@ -0,0 +1,13 @@
+import Logo from '@/UI/Btn/Logo';
+import React, { FC } from 'react';
+import './styles.scss';
+
+const page: FC = () => {
+  return (
+    <div className="splash">
+      <Logo color="#fff" />
+    </div>
+  );
+};
+
+export default page;
